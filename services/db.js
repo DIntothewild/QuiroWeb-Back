ad91f6fb62a1b00b7ab7ce2ae260ab36d34a31df
DIntothewild <drviajando@gmail.com>
2025-04-12 13:19:25 +0200

    Conexión MongoDB Atlas configurada

--- services/db.js
+++ services/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const mongodbConnection = async () => {
   mongoose.set("strictQuery", false);
-  const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  const uri = process.env.MONGODB_URI;
   try {
     await mongoose.connect(uri);
     console.log("Connected to MongoDB");
