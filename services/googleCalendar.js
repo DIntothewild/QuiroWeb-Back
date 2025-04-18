a8abe8f27707c91230c694d24c31d4a628e9cc37
DIntothewild <drviajando@gmail.com>
2025-04-18 10:50:19 +0200

    Fix: Google Calendar JWT auth para evitar ERR_OSSL_UNSUPPORTED

--- services/googleCalendar.js
+++ services/googleCalendar.js
@@ -4,8 +4,9 @@ const path = require("path");
 
 const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-const auth = new google.auth.GoogleAuth({
-  credentials,
+const auth = new google.auth.JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
